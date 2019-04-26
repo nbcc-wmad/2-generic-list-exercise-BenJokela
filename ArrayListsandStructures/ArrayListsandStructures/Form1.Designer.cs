48d8f05252d3f6a4273e8033b35035189259b4e2
BenJokela <benjokela@gmail.com>
2019-04-26 11:44:18 -0300

    first commit

---
+++ ArrayListsandStructures/ArrayListsandStructures/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ArrayListsandStructures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.btnFindGrade = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(148, 104);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(42, 15);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(220, 104);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 1;
+            // 
+            // btnFindGrade
+            // 
+            this.btnFindGrade.Location = new System.Drawing.Point(188, 158);
+            this.btnFindGrade.Name = "btnFindGrade";
+            this.btnFindGrade.Size = new System.Drawing.Size(111, 23);
+            this.btnFindGrade.TabIndex = 2;
+            this.btnFindGrade.Text = "Find Grade";
+            this.btnFindGrade.UseVisualStyleBackColor = true;
+            this.btnFindGrade.Click += new System.EventHandler(this.btnFindGrade_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 288);
+            this.Controls.Add(this.btnFindGrade);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button btnFindGrade;
+    }
+}
+
---
+++ ArrayListsandStructures/ArrayListsandStructures/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayListsandStructures
+{
+    public partial class Form1 : Form
+    {
+        private struct Grade
+        {
+            public int MaxScore;
+            public string LetterGrade;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private ArrayList Grades = new ArrayList();
+
+        private Grade grade;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillGradesList(299, "F");
+            FillGradesList(349, "D");
+            FillGradesList(399, "C");
+            FillGradesList(449, "B");
+            FillGradesList(499, "A");
+        }
+        private void FillGradesList(int maxScore, string letterGrade)
+        {
+            grade.MaxScore = maxScore;
+            grade.LetterGrade = letterGrade;
+            Grades.Add(grade);
+        }
+        private void btnFindGrade_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtScore.Text, out int score))
+            {
+                MessageBox.Show("Enter a number dumbass!");
+            }
+            else
+            {
+                int entry = Convert.ToInt16(txtScore.Text);
+
+            }
+
+        }
+
+    }
+}
+
+
+
+
